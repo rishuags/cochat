4871ce987b87a6bd6bf12a5f435eb0b8456fcd03
Rishu <agrawalrishu8@gmail.com>
2025-07-30 10:52:06 +0100

    Removing Commented Code

--- cochat-frontend/src/App.jsx
+++ cochat-frontend/src/App.jsx
@@ -1,36 +1,3 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'*/
-
-/*import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import MyList from './MyList.jsx'
-import Card from './Card.jsx'
-import AuthForm from './AuthForm.jsx';
-
-
-
-function App() {
-  
-
-
-  return (
-    <>
-      <AuthForm />
-      <Header />
-      <MyList />
-      <Card />
-      <Footer />
-    </>
-
-  )
-
-}
-
-export default App */
-
-
 
 import { useEffect, useState } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
